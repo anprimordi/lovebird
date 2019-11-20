6ff61c7c2fbbd8ef176f9ff2f4e5354e30f3a7f4
Ahmad N. Primordi <ahmadnuftahprimordi@gmail.com>
2019-11-20 20:23:50 +0700

    update hasil activity, add hasil adapter

--- app/src/main/java/com/makaryostudio/lovebird/feature/diagnosis/DiagnosisAdapter.kt
+++ app/src/main/java/com/makaryostudio/lovebird/feature/diagnosis/DiagnosisAdapter.kt
@@ -15,16 +15,16 @@ class DiagnosisAdapter internal constructor(private val context: Context) :
     RecyclerView.Adapter<DiagnosisAdapter.ViewHolder>() {
 
     private var mCheckedGejala: ArrayList<Gejala> = ArrayList()
-    private var inputGejala : ArrayList<Int> = ArrayList()
+    private var inputGejala: ArrayList<Int> = ArrayList()
 
     //Array Penyakit
-    private var cacingan : ArrayList<Int> = arrayListOf(0,1,2,3,4)
-    private var kutuBurung : ArrayList<Int> = arrayListOf(0,4,5,6,9)
-    private var snot : ArrayList<Int> = arrayListOf(0,7,8,11,12)
-    private var berakKapur : ArrayList<Int> = arrayListOf(0,1,10,17)
-    private var mencretBakteri : ArrayList<Int> = arrayListOf(0,1,2)
-    private var bubul : ArrayList<Int> = arrayListOf(13,14)
-    private var virusBurung: ArrayList<Int> = arrayListOf(15,16)
+    private var cacingan: ArrayList<Int> = arrayListOf(0, 1, 2, 3, 4)
+    private var kutuBurung: ArrayList<Int> = arrayListOf(0, 4, 5, 6, 9)
+    private var snot: ArrayList<Int> = arrayListOf(0, 7, 8, 11, 12)
+    private var berakKapur: ArrayList<Int> = arrayListOf(0, 1, 10, 17)
+    private var mencretBakteri: ArrayList<Int> = arrayListOf(0, 1, 2)
+    private var bubul: ArrayList<Int> = arrayListOf(13, 14)
+    private var virusBurung: ArrayList<Int> = arrayListOf(15, 16)
 
     private var mListData: List<Gejala>
 
@@ -52,7 +52,7 @@ class DiagnosisAdapter internal constructor(private val context: Context) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val gejala = mListData[position]
 
-        holder.mCbGejala.text = gejala.gejalaDialami.toString()
+        holder.mCbGejala.text = gejala.gejalaDialami
         holder.mCbGejala.isChecked = gejala.isChecked
 
         holder.mCbGejala.setOnClickListener {
@@ -69,8 +69,6 @@ class DiagnosisAdapter internal constructor(private val context: Context) :
                     println(element)
                 }
 
-
-
             } else {
                 holder.mCbGejala.isChecked = false
                 gejala.isChecked = false
@@ -78,64 +76,56 @@ class DiagnosisAdapter internal constructor(private val context: Context) :
                 inputGejala.remove(gejala.kodeGejala)
             }
 
-
             intent.putExtra("checked", mCheckedGejala)
             intent.putExtra("gejala", gejala)
         }
-
-
-
-
     }
-    fun cbr(){
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var mCbGejala: CheckBox = itemView.findViewById(R.id.checkbox_gejala)
+    }
+
+    fun cbr() {
+        var hs1: Float = 0f
+        var hs2: Float = 0f
+        var hs3: Float = 0f
+        var hs4: Float = 0f
+        var hs5: Float = 0f
+        var hs6: Float = 0f
+        var hs7: Float = 0f
 
 
-        var hs1:Float =0f
-        var hs2:Float =0f
-        var hs3:Float =0f
-        var hs4:Float =0f
-        var hs5:Float =0f
-        var hs6:Float =0f
-        var hs7:Float =0f
+        var tb1: Float = 0f
+        var tb2: Float = 0f
+        var tb3: Float = 0f
+        var tb4: Float = 0f
+        var tb5: Float = 0f
+        var tb6: Float = 0f
+        var tb7: Float = 0f
 
 
+        var tbp1: Float = 0f
+        var tbp2: Float = 0f
+        var tbp3: Float = 0f
+        var tbp4: Float = 0f
+        var tbp5: Float = 0f
+        var tbp6: Float = 0f
+        var tbp7: Float = 0f
 
 
-        var tb1:Float =0f
-        var tb2:Float =0f
-        var tb3:Float =0f
-        var tb4:Float =0f
-        var tb5:Float =0f
-        var tb6:Float =0f
-        var tb7:Float =0f
-
-
-        var tbp1:Float =0f
-        var tbp2:Float =0f
-        var tbp3:Float =0f
-        var tbp4:Float =0f
-        var tbp5:Float =0f
-        var tbp6:Float =0f
-        var tbp7:Float =0f
-
-
-
-        var Penyakit: String=""
-        var persentaseCBR: Float=0f
-        var bobotMax: Float=0f
-
+        var Penyakit: String = ""
+        var persentaseCBR: Float = 0f
+        var bobotMax: Float = 0f
 
         //Penyakit 1
 
-       for (i in 0 until cacingan.size){
+        for (i in 0 until cacingan.size) {
 
-           //total bobot
-           tbp1 += mListData[cacingan[i]].bobot
-            for(j in 0 until inputGejala.size)
-            {
+            //total bobot
+            tbp1 += mListData[cacingan[i]].bobot
+            for (j in 0 until inputGejala.size) {
 
-                if(cacingan[i]==inputGejala[j])
-                {
+                if (cacingan[i] == inputGejala[j]) {
                     tb1 += mListData[cacingan[i]].bobot
 
                 }
@@ -143,14 +133,12 @@ class DiagnosisAdapter internal constructor(private val context: Context) :
         }
 
         //Penyakit 2
-        for (i in 0 until kutuBurung.size){
+        for (i in 0 until kutuBurung.size) {
 
             //total bobot
             tbp2 += mListData[kutuBurung[i]].bobot
-            for(j in 0 until inputGejala.size)
-            {
-                if(kutuBurung[i]==inputGejala[j])
-                {
+            for (j in 0 until inputGejala.size) {
+                if (kutuBurung[i] == inputGejala[j]) {
                     tb2 += mListData[kutuBurung[i]].bobot
 
                 }
@@ -159,14 +147,12 @@ class DiagnosisAdapter internal constructor(private val context: Context) :
 
 
         //Penyakit 3
-        for (i in 0 until snot.size){
+        for (i in 0 until snot.size) {
 
             //total bobot
             tbp3 += mListData[snot[i]].bobot
-            for(j in 0 until inputGejala.size)
-            {
-                if(snot[i]==inputGejala[j])
-                {
+            for (j in 0 until inputGejala.size) {
+                if (snot[i] == inputGejala[j]) {
                     tb3 += mListData[snot[i]].bobot
 
                 }
@@ -175,14 +161,12 @@ class DiagnosisAdapter internal constructor(private val context: Context) :
 
 
         //Penyakit 4
-        for (i in 0 until berakKapur.size){
+        for (i in 0 until berakKapur.size) {
 
             //total bobot
             tbp4 += mListData[berakKapur[i]].bobot
-            for(j in 0 until inputGejala.size)
-            {
-                if(berakKapur[i]==inputGejala[j])
-                {
+            for (j in 0 until inputGejala.size) {
+                if (berakKapur[i] == inputGejala[j]) {
                     tb4 += mListData[berakKapur[i]].bobot
 
                 }
@@ -190,192 +174,160 @@ class DiagnosisAdapter internal constructor(private val context: Context) :
         }
 
         //Penyakit 5
-        for (i in 0 until mencretBakteri.size){
+        for (i in 0 until mencretBakteri.size) {
 
             //total bobot
             tbp5 += mListData[mencretBakteri[i]].bobot
-            for(j in 0 until inputGejala.size)
-            {
-                if(mencretBakteri[i]==inputGejala[j])
-                {
+            for (j in 0 until inputGejala.size) {
+                if (mencretBakteri[i] == inputGejala[j]) {
                     tb5 += mListData[mencretBakteri[i]].bobot
-
                 }
             }
         }
 
 
         //Penyakit 6
-        for (i in 0 until bubul.size){
+        for (i in 0 until bubul.size) {
 
             //total bobot
             tbp6 += mListData[bubul[i]].bobot
-            for(j in 0 until inputGejala.size)
-            {
-                if(bubul[i]==inputGejala[j])
-                {
+            for (j in 0 until inputGejala.size) {
+                if (bubul[i] == inputGejala[j]) {
                     tb6 += mListData[bubul[i]].bobot
-
                 }
             }
         }
 
 
         //Penyakit 7
-        for (i in 0 until virusBurung.size){
+        for (i in 0 until virusBurung.size) {
 
             //total bobot
             tbp7 += mListData[virusBurung[i]].bobot
-            for(j in 0 until inputGejala.size)
-            {
-                if(virusBurung[i]==inputGejala[j])
-                {
+            for (j in 0 until inputGejala.size) {
+                if (virusBurung[i] == inputGejala[j]) {
                     tb7 += mListData[virusBurung[i]].bobot
 
                 }
             }
         }
 
-
-        hs1=(tb1/tbp1)*100
-        hs2=(tb2/tbp2)*100
-        hs3=(tb3/tbp3)*100
-        hs4=(tb4/tbp4)*100
-        hs5=(tb5/tbp5)*100
-        hs6=(tb6/tbp7)*100
-        hs7=(tb7/tbp7)*100
-        println("Hasil : "+hs1)
-        println("TB : "+ tb1)
-        println("TBP : "+ tbp1)
-        println("Hasil : "+hs2)
-        println("TB : "+ tb2)
-        println("TBP : "+ tbp2)
-        println("Hasil : "+hs3)
-        println("TB : "+ tb3)
-        println("TBP : "+ tbp3)
-        println("Hasil : "+hs4)
-        println("TB : "+ tb4)
-        println("TBP : "+ tbp4)
-        println("Hasil : "+hs5)
-        println("TB : "+ tb5)
-        println("TBP : "+ tbp5)
-        println("Hasil : "+hs6)
-        println("TB : "+ tb6)
-        println("TBP : "+ tbp6)
-        println("Hasil : "+hs7)
-        println("TB : "+ tb7)
-        println("TBP : "+ tbp7)
-
-
+        hs1 = (tb1 / tbp1) * 100
+        hs2 = (tb2 / tbp2) * 100
+        hs3 = (tb3 / tbp3) * 100
+        hs4 = (tb4 / tbp4) * 100
+        hs5 = (tb5 / tbp5) * 100
+        hs6 = (tb6 / tbp7) * 100
+        hs7 = (tb7 / tbp7) * 100
+        println("Hasil : $hs1")
+        println("TB : $tb1")
+        println("TBP : $tbp1")
+        println("Hasil : $hs2")
+        println("TB : $tb2")
+        println("TBP : $tbp2")
+        println("Hasil : $hs3")
+        println("TB : $tb3")
+        println("TBP : $tbp3")
+        println("Hasil : $hs4")
+        println("TB : $tb4")
+        println("TBP : $tbp4")
+        println("Hasil : $hs5")
+        println("TB : $tb5")
+        println("TBP : $tbp5")
+        println("Hasil : $hs6")
+        println("TB : $tb6")
+        println("TBP : $tbp6")
+        println("Hasil : $hs7")
+        println("TB : $tb7")
+        println("TBP : $tbp7")
 
         //Searching paling besar
-        if (hs1>persentaseCBR){
-            persentaseCBR=hs1
-            Penyakit="Cacingan"
-            bobotMax=tb1
-        }
-        else if(hs1==persentaseCBR) {
-            if (bobotMax < tb1)
-            {
-                persentaseCBR=hs1
-                Penyakit="Cacingan"
-                bobotMax=tb1
+        if (hs1 > persentaseCBR) {
+            persentaseCBR = hs1
+            Penyakit = "Cacingan"
+            bobotMax = tb1
+        } else if (hs1 == persentaseCBR) {
+            if (bobotMax < tb1) {
+                persentaseCBR = hs1
+                Penyakit = "Cacingan"
+                bobotMax = tb1
             }
         }
 
-        if (hs2>persentaseCBR){
-            persentaseCBR=hs2
-            Penyakit="Kutu Burung"
-            bobotMax=tb2
-        }
-        else if(hs2==persentaseCBR) {
-            if (bobotMax < tb2)
-            {
-                persentaseCBR=hs2
-                Penyakit="Kutu Burung"
-                bobotMax=tb2
+        if (hs2 > persentaseCBR) {
+            persentaseCBR = hs2
+            Penyakit = "Kutu Burung"
+            bobotMax = tb2
+        } else if (hs2 == persentaseCBR) {
+            if (bobotMax < tb2) {
+                persentaseCBR = hs2
+                Penyakit = "Kutu Burung"
+                bobotMax = tb2
             }
         }
 
-        if (hs3>persentaseCBR){
-            persentaseCBR=hs3
-            Penyakit="Snot"
-            bobotMax=tb3
-        }
-        else if(hs3==persentaseCBR) {
-            if (bobotMax < tb3)
-            {
-                persentaseCBR=hs3
-                Penyakit="Snot"
-                bobotMax=tb3
+        if (hs3 > persentaseCBR) {
+            persentaseCBR = hs3
+            Penyakit = "Snot"
+            bobotMax = tb3
+        } else if (hs3 == persentaseCBR) {
+            if (bobotMax < tb3) {
+                persentaseCBR = hs3
+                Penyakit = "Snot"
+                bobotMax = tb3
             }
         }
 
-        if (hs4>persentaseCBR){
-            persentaseCBR=hs4
-            Penyakit="Berak Kapur"
-            bobotMax=tb4
-        }
-        else if(hs4==persentaseCBR) {
-            if (bobotMax < tb4)
-            {
-                persentaseCBR=hs4
-                Penyakit="Berak Kapur"
-                bobotMax=tb4
+        if (hs4 > persentaseCBR) {
+            persentaseCBR = hs4
+            Penyakit = "Berak Kapur"
+            bobotMax = tb4
+        } else if (hs4 == persentaseCBR) {
+            if (bobotMax < tb4) {
+                persentaseCBR = hs4
+                Penyakit = "Berak Kapur"
+                bobotMax = tb4
             }
         }
 
-        if (hs5>persentaseCBR){
-            persentaseCBR=hs5
-            Penyakit="Mencret Bakteri"
-            bobotMax=tb5
-        }
-        else if(hs5==persentaseCBR) {
-            if (bobotMax < tb5)
-            {
-                persentaseCBR=hs5
-                Penyakit="Mencret Bakteri"
-                bobotMax=tb5
+        if (hs5 > persentaseCBR) {
+            persentaseCBR = hs5
+            Penyakit = "Mencret Bakteri"
+            bobotMax = tb5
+        } else if (hs5 == persentaseCBR) {
+            if (bobotMax < tb5) {
+                persentaseCBR = hs5
+                Penyakit = "Mencret Bakteri"
+                bobotMax = tb5
             }
         }
 
-        if (hs6>persentaseCBR){
-            persentaseCBR=hs6
-            Penyakit="Bubul"
-            bobotMax=tb6
-        }
-        else if(hs6==persentaseCBR) {
-            if (bobotMax < tb6)
-            {
-                persentaseCBR=hs6
-                Penyakit="Bubul"
-                bobotMax=tb6
+        if (hs6 > persentaseCBR) {
+            persentaseCBR = hs6
+            Penyakit = "Bubul"
+            bobotMax = tb6
+        } else if (hs6 == persentaseCBR) {
+            if (bobotMax < tb6) {
+                persentaseCBR = hs6
+                Penyakit = "Bubul"
+                bobotMax = tb6
             }
         }
 
-        if (hs7>persentaseCBR){
-            persentaseCBR=hs7
-            Penyakit="Virus Burung"
-            bobotMax=tb7
-        }
-        else if(hs7==persentaseCBR) {
-            if (bobotMax < tb7)
-            {
-                persentaseCBR=hs7
-                Penyakit="Virus Burung"
-                bobotMax=tb7
+        if (hs7 > persentaseCBR) {
+            persentaseCBR = hs7
+            Penyakit = "Virus Burung"
+            bobotMax = tb7
+        } else if (hs7 == persentaseCBR) {
+            if (bobotMax < tb7) {
+                persentaseCBR = hs7
+                Penyakit = "Virus Burung"
+                bobotMax = tb7
             }
         }
 
-        println("Diagnosa Penyakit : "+Penyakit)
-        println("Persentase CBR : "+persentaseCBR)
-
+        println("Diagnosa Penyakit : " + Penyakit)
+        println("Persentase CBR : " + persentaseCBR)
 
     }
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var mCbGejala: CheckBox = itemView.findViewById(R.id.checkbox_gejala)
-    }
-
-
-
 }
--- app/src/main/java/com/makaryostudio/lovebird/feature/hasil/HasilActivity.kt
+++ app/src/main/java/com/makaryostudio/lovebird/feature/hasil/HasilActivity.kt
@@ -2,12 +2,35 @@ package com.makaryostudio.lovebird.feature.hasil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.makaryostudio.lovebird.R
+import com.makaryostudio.lovebird.repository.model.Gejala
 
 class HasilActivity : AppCompatActivity() {
+
+    private lateinit var textNamaBurung: TextView
+    private lateinit var adapter: RecyclerView
+    private lateinit var textHasilDiagnosis: TextView
+    private lateinit var textSolusi: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hasil)
+
+        val gejala = intent.getParcelableExtra<Gejala>("gejala")
+        val checked = intent.getBooleanArrayExtra("checked")
+        val namaBurung = intent.getStringExtra("namaBurung")
+
+        textNamaBurung.text = namaBurung
+
+        bindView()
+    }
+
+    private fun bindView() {
+        textNamaBurung = findViewById(R.id.text_nama_burung)
+        adapter = findViewById(R.id.rv_gejala_dialami)
+        textHasilDiagnosis = findViewById(R.id.text_hasil_diagnosis)
+        textSolusi = findViewById(R.id.text_solusi)
     }
 }
