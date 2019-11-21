b0415435ae6962679465ce0423ed6de7493d50b7
alamasrorul <alamasrorulhaqqracer@gmail.com>
2019-11-21 07:19:13 +0700

    CBR Function Is Activated

--- app/src/main/java/com/makaryostudio/lovebird/feature/hasil/HasilActivity.kt
+++ app/src/main/java/com/makaryostudio/lovebird/feature/hasil/HasilActivity.kt
@@ -2,12 +2,15 @@ package com.makaryostudio.lovebird.feature.hasil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import com.makaryostudio.lovebird.R
+
 
 class HasilActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hasil)
+
     }
 }
