5fa0f8fa19fe24d10c0dd256de872b788c312b95
Ahmad N. Primordi <ahmadnuftahprimordi@gmail.com>
2019-11-27 16:12:34 +0700

    update recyclerview gejala item

--- app/src/main/java/com/makaryostudio/lovebird/feature/diagnosis/DiagnosisActivity.kt
+++ app/src/main/java/com/makaryostudio/lovebird/feature/diagnosis/DiagnosisActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.makaryostudio.lovebird.R
